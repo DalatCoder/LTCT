8cce58c634b6c75b6dd5b4311d0396533a7b1563
DalatCoder <hieuntctk42@gmail.com>
2019-04-14 13:58:07 +0700

    Complete project Lab06_D_Bai3

--- 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai3/Lab06_D_Bai3/program.cpp
+++ 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai3/Lab06_D_Bai3/program.cpp
@@ -16,6 +16,22 @@ int main()
 
 void ChayChuongTrinh()
 {
-
+	char kt;
+	int a[MAX][MAX], n;
+	do
+	{
+		system("cls");
+		cout << "\nNhap n = ";
+		cin >> n;
+		Tao_MaTran_XoanOc(a, n);
+		system("cls");
+		cout << "\nTao ma tran xoan oc cap " << n << " : ";
+		XuatMaTran(a, n);
+		_getch();
+		cout << "\n\nNua khong, nhan ESC neu khong!\n";
+		kt = _getch();
+	} while (kt != 27);
 	_getch();
 }
+
+
--- 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai3/Lab06_D_Bai3/thuvien.h
+++ 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai3/Lab06_D_Bai3/thuvien.h
@@ -1,6 +1,7 @@
 #define MAX 10
 
 void XuatMaTran(int a[MAX][MAX], int n);
+void Tao_MaTran_XoanOc(int a[MAX][MAX], int n);
 
 void XuatMaTran(int a[MAX][MAX], int n)
 {
@@ -12,4 +13,56 @@ void XuatMaTran(int a[MAX][MAX], int n)
 			cout << setw(5) << a[i][j];
 	}
 }
+
+void Tao_MaTran_XoanOc(int a[MAX][MAX], int n)
+{
+	int value, hangTren, hangDuoi, cotTrai, cotPhai;
+	int i, j;
+
+	hangTren = 0;
+	cotPhai = n - 1;
+	hangDuoi = n - 1;
+	cotTrai = 0;
+	value = 1;
+
+	while (value <= n * n)
+	{
+		for (j = cotTrai; (j <= cotPhai) && (value <= n*n); j++)
+		{
+			a[hangTren][j] = value;
+			value++;
+		}
+		if (value > n*n)
+			break;
+		hangTren++;
+
+		for (i = hangTren; (i <= hangDuoi) && (value <= n*n); i++)
+		{
+			a[i][cotPhai] = value;
+			value++;
+		}
+		if (value > n*n)
+			break;
+		cotPhai--;
+
+		for (j = cotPhai; (j >= cotTrai) && (value <= n*n); j--)
+		{
+			a[hangDuoi][j] = value;
+			value++;
+		}
+		if (value > n*n)
+			break;
+		hangDuoi--;
+
+		for (i = hangDuoi; (i >= hangTren) && (value <= n*n); i--)
+		{
+			a[i][cotTrai] = value;
+			value++;
+		}
+		if (value > n*n)
+			break;
+		cotTrai++;
+	}
+}
+
 
