cd0bed6e2a429f2c203ad8e8b080fa4a41bf09cd
DalatCoder <hieuntctk42@gmail.com>
2019-04-16 11:14:55 +0700

    Add function NhapMaTran and XuatMaTran

--- 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai4/Lab06_D_Bai4/Program.cpp
+++ 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai4/Lab06_D_Bai4/Program.cpp
@@ -1,11 +1,17 @@
 #include <iostream>
 #include <conio.h>
+#include <iomanip>
 
 using namespace std;
 
+#include "Thuvien.h"
+
 int main()
 {
-
+	MaTranVuong a;
+	int n = 5;
+	NhapMaTran(a, n, 'A');
+	XuatMaTran(a, n);
 	_getch();
 	return 0;
 }
--- 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai4/Lab06_D_Bai4/Thuvien.h
+++ 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai4/Lab06_D_Bai4/Thuvien.h
@@ -2,4 +2,30 @@
 
 typedef int MaTranVuong[SIZE][SIZE];
 
+void NhapMaTran(MaTranVuong a, int n, char kt);
+void XuatMaTran(MaTranVuong a, int n);
+
+void NhapMaTran(MaTranVuong a, int n, char kt)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+		{
+			cout << "\n" << kt << "[" << i << "][" << j << "] = ";
+			cin >> a[i][j];
+		}
+}
+
+void XuatMaTran(MaTranVuong a, int n)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		cout << endl << endl;
+		for (j = 0; j < n; j++)
+			cout << setw(4) << a[i][j];
+	}
+}
+
+
 
