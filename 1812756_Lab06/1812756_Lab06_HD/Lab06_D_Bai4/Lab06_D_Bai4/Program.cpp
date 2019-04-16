1b04a2182cddf9f7dd44fc1371b5ad98b6b66fbc
DalatCoder <hieuntctk42@gmail.com>
2019-04-16 11:09:19 +0700

    Add Project Lab06_D_Bai4

---
+++ 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai4/Lab06_D_Bai4/Program.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+
+	_getch();
+	return 0;
+}
---
+++ 1812756_Lab06/1812756_Lab06_HD/Lab06_D_Bai4/Lab06_D_Bai4/Thuvien.h
@@ -0,0 +1,5 @@
+#define SIZE 10
+
+typedef int MaTranVuong[SIZE][SIZE];
+
+
