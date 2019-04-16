b25e4a300176678350a5ef0991364cbafeacb799
DalatCoder <hieuntctk42@gmail.com>
2019-04-16 11:40:51 +0700

    Add function NhapMaTran and XuatMaTran

--- 1812756_Lab06/1812756_Lab06_BB/Lab06_E_Bai01/Lab06_E_Bai01/Program.cpp
+++ 1812756_Lab06/1812756_Lab06_BB/Lab06_E_Bai01/Lab06_E_Bai01/Program.cpp
@@ -9,6 +9,19 @@ using namespace std;
 int main()
 {
 
+	MaTranVuong a;
+	int n;
+
+	do
+	{
+		cout << "\nNhap kich thuoc ma tran (0.." << SIZE - 1 << ") : ";
+		cin >> n;
+	} while (n < 0 || n >= SIZE);
+
+	cout << "\nMa tran ngau nhien :\n";
+	NhapMaTran_TD(a, n);
+	XuatMaTran(a, n);
+
 	_getch();
 	return 0;
 }
--- 1812756_Lab06/1812756_Lab06_BB/Lab06_E_Bai01/Lab06_E_Bai01/Thuvien.h
+++ 1812756_Lab06/1812756_Lab06_BB/Lab06_E_Bai01/Lab06_E_Bai01/Thuvien.h
@@ -1,4 +1,47 @@
 #define SIZE 10
 
-typedef int MaTranVuong[SIZE][SIZE];
+typedef double MaTranVuong[SIZE][SIZE];
+
+void NhapMaTran_TD(MaTranVuong a, int n);
+void XuatMaTran(MaTranVuong a, int n);
+void NhapMaTran(MaTranVuong a, int n);
+
+double fRand(double fMin, double fMax)
+{
+	double f = (double)rand() / RAND_MAX;
+	return fMin + f * (fMax - fMin);
+}
+
+void NhapMaTran_TD(MaTranVuong a, int n)
+{
+	srand((unsigned)time(NULL));
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+			a[i][j] = fRand(1.0, 10.0);
+}
+
+void NhapMaTran(MaTranVuong a, int n)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+		for (j = 0; j < n; j++)
+		{
+			cout << "a[" << i << "][" << j << "] = ";
+			cin >> a[i][j];
+		}
+}
+
+void XuatMaTran(MaTranVuong a, int n)
+{
+	int i, j;
+	for (i = 0; i < n; i++)
+	{
+		cout << "\n";
+		for (j = 0; j < n; j++)
+			cout << setw(7) << setprecision(3) << a[i][j];
+	}
+	cout << "\n";
+}
+
 
