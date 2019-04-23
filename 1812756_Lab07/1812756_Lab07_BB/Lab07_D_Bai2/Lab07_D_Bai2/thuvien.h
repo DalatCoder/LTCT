dc84985fbed2fbd99c961ccbf73d7bf5becc3ade
DalatCoder <hieuntctk42@gmail.com>
2019-04-23 15:45:47 +0700

    Them ham chuyen kt thuong -> hoa va nguoc lai

--- 1812756_Lab07/1812756_Lab07_BB/Lab07_D_Bai2/Lab07_D_Bai2/thuvien.h
+++ 1812756_Lab07/1812756_Lab07_BB/Lab07_D_Bai2/Lab07_D_Bai2/thuvien.h
@@ -2,3 +2,16 @@
 #define MAX 100
 #define CACH ''
 
+char Chuyen_KT_Thuong(char x)
+{
+	if ('A' <= x && x <= 'Z')
+		x = x + 32;
+	return x;
+}
+
+char Chuyen_KT_Hoa(char x)
+{
+	if ('a' <= x && x <= 'z')
+		x = x - 32;
+	return x;
+}
