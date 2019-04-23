b7dcc2b6e5274a07c4921ca5ff8bc9e79ff02dbb
DalatCoder <hieuntctk42@gmail.com>
2019-04-23 15:43:41 +0700

    Project nan ten

---
+++ 1812756_Lab07/1812756_Lab07_BB/Lab07_D_Bai2/Lab07_D_Bai2/program.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+#include "thuvien.h"
+
+void ChayChuongTrinh();
+
+int main()
+{
+	ChayChuongTrinh();
+	return 0;
+}
+
+void ChayChuongTrinh()
+{
+
+	_getch();
+}
