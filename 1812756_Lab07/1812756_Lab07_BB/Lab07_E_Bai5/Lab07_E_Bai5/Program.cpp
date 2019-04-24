157a774ad29bcf474206000f046ab263037839af
DalatCoder <hieuntctk42@gmail.com>
2019-04-24 08:03:31 +0700

    Tu viet ham chuyen doi

--- 1812756_Lab07/1812756_Lab07_BB/Lab07_E_Bai5/Lab07_E_Bai5/Program.cpp
+++ 1812756_Lab07/1812756_Lab07_BB/Lab07_E_Bai5/Lab07_E_Bai5/Program.cpp
@@ -1,4 +1,4 @@
-// Chuyen xau ky tu thanh gia tri so tuong ung
+﻿// Chuyen xau ky tu thanh gia tri so tuong ung
 
 #include <iostream>
 #include <conio.h>
@@ -7,23 +7,64 @@
 
 using namespace std;
 
+int KyTuSangSo(char x);
 int ChuyenDoi(char a[MAX]);
 
 int main()
 {
-	char a[MAX] = "1234567";
-	int kq = ChuyenDoi(a);
-	cout << kq + 1<< endl;
+	char a[MAX] = "1234567890";
+	int kq;
+	kq = ChuyenDoi(a);
+	cout << "\nDang so cua chuoi a la : " << kq;
+
+	// Chứng minh kq là 1 số bằng cách thực hiện phép toán lên kq
+	int x = kq + 1;
+	int y = kq - 1;
+
+	cout << "\nx = " << x;
+	cout << "\ny = " << y;
+
+	_getch();
 	return 0;
 }
 
 int ChuyenDoi(char a[MAX])
 {
-	int kq = 1;
+	int kq, i;
+	kq = 0;
 
-	kq = atoi(a);
+	for (i = 0; a[i] != NULL; i++)
+	{
+		kq = kq * 10 + KyTuSangSo(a[i]);
+	}
 
 	return kq;
 }
 
+int KyTuSangSo(char x)
+{
+	switch (x)
+	{
+	case '0':
+		return 0;
+	case '1':
+		return 1;
+	case '2':
+		return 2;
+	case '3':
+		return 3;
+	case '4':
+		return 4;
+	case '5':
+		return 5;
+	case '6':
+		return 6;
+	case '7':
+		return 7;
+	case '8':
+		return 8;
+	case '9':
+		return 9;
+	}
+}
 
