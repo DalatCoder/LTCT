cd6f22bcbd5cc21a5750c05ff0e4d5f16b69deb0
DalatCoder <hieuntctk42@gmail.com>
2019-04-26 12:17:32 +0700

    Bo sung ham tim thu ngay trong tuan

--- Exam/1812756_NguyenTrongHieu_BS_BT2/1812756_NguyenTrongHieu_BS_BT2/1812756_Thuvien.h
+++ Exam/1812756_NguyenTrongHieu_BS_BT2/1812756_NguyenTrongHieu_BS_BT2/1812756_Thuvien.h
@@ -3,6 +3,9 @@
 
 typedef int maTran[MAX][MAX];
 
+void KhoiTao(maTran lich);
+int TimThuTrongTuan(int m, int y);
+
 // Khoi tao tat ca phan tu cua ma tran ban dau bang 0
 void KhoiTao(maTran lich)
 {
@@ -12,6 +15,15 @@ void KhoiTao(maTran lich)
 			lich[i][j] = 0;
 }
 
+int TimThuTrongTuan(int m, int y)
+{
+	int t, x, k, w;
+	t = y - (14 - m) / 12;
+	x = t + t / 4 - t / 100 + t / 400;
+	k = m + 12 * ((14 - m) / 12) - 2;
+	w = (1 + x + (31 * k) / 12) % 7;
+	return w;
+}
 
 
 
