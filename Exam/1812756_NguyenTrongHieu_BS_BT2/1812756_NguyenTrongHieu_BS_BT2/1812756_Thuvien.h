77ef3767ef5bf871c91518637b0489dbf5bc0ea4
DalatCoder <hieuntctk42@gmail.com>
2019-04-26 13:02:25 +0700

    Bo sung ham xuat tieu de

--- Exam/1812756_NguyenTrongHieu_BS_BT2/1812756_NguyenTrongHieu_BS_BT2/1812756_Thuvien.h
+++ Exam/1812756_NguyenTrongHieu_BS_BT2/1812756_NguyenTrongHieu_BS_BT2/1812756_Thuvien.h
@@ -12,6 +12,7 @@ int SoNgayTrongThang(int m, int y);
 void TaoLich(maTran lich, int m, int y);
 void XuatLich_CN(maTran lich, int m);
 void XuatLich_T2(maTran lich, int m);
+void XuatTieuDe(int m, int y);
 
 int NhapThang()
 {
@@ -191,3 +192,51 @@ void XuatLich_T2(maTran lich, int m)
 		cout << endl;
 	}
 }
+
+void XuatTieuDe(int m, int y)
+{
+	cout << endl;
+	switch (m)
+	{
+		case 1:
+			cout << "January";
+			break;
+		case 2:
+			cout << "February";
+			break;
+		case 3:
+			cout << "March";
+			break;
+		case 4:
+			cout << "April";
+			break;
+		case 5:
+			cout << "May";
+			break;
+		case 6:
+			cout << "June";
+			break;
+		case 7:
+			cout << "July";
+			break;
+		case 8: 
+			cout << "August";
+			break;
+		case 9:
+			cout << "September";
+			break;
+		case 10:
+			cout << "October";
+			break;
+		case 11:
+			cout << "November";
+			break;
+		case 12:
+			cout << "December";
+			break;
+		}
+		cout << " " << y << endl;
+}
+
+
+
