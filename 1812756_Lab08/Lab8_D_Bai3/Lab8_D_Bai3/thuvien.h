2eea286f2201ea05ef60f2780df9764829333334
DalatCoder <hieuntctk42@gmail.com>
2019-05-06 00:19:20 +0700

    Chuong trinh in bang diem mon LTCT

---
+++ 1812756_Lab08/Lab8_D_Bai3/Lab8_D_Bai3/thuvien.h
@@ -0,0 +1,23 @@
+
+struct SinhVien
+{
+	char maSV[8];
+	char hoLot[15];
+	char ten[8];
+	char gioiTinh[4];
+	unsigned int namSinh;
+	char queQuan[15];
+	char lop[6];
+};
+
+struct BangDiem_ChiTiet
+{
+	char maSV[8];
+	double diemLab;
+	double baiKTGK;
+	double baiKTCK;
+	double diemTK;
+};
+
+
+
