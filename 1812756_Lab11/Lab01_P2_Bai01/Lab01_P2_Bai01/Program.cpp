6c607e2d8685d6a746a27f8be0b2cd0155507d80
DalatCoder <hieuntctk42@gmail.com>
2019-05-16 14:27:49 +0700

    Bo sung ham tu dong them duoi .txt

--- 1812756_Lab11/Lab01_P2_Bai01/Lab01_P2_Bai01/Program.cpp
+++ 1812756_Lab11/Lab01_P2_Bai01/Lab01_P2_Bai01/Program.cpp
@@ -19,9 +19,22 @@ void ChayChuongTrinh()
 {
 	int n, a[MAX];
 	char filename[MAX];
+
 	system("cls");
 	cout << "Nhap ten file mo de doc: ";
 	cin >> filename;
+
+	// Them duoi .txt vao sau filename
+	int dau = 0;
+	for (int i = 0; filename[i] != NULL; i++)
+		if (filename[i] == '.')
+		{
+			dau = 1;
+			break;
+		}
+	if (!dau)
+		strcat_s(filename, ".txt");
+
 	File_Array(filename, a, n);
 	cout << n << endl;
 	for (int i = 0; i < n; i++)
