037ce3106f4db8fe44abef82e187e85f163b310f
DalatCoder <hieuntctk42@gmail.com>
2019-05-16 14:20:47 +0700

    Bo sung ham doc file theo cach tra ve gia tri

--- 1812756_Lab11/Lab01_P2_Bai01/Lab01_P2_Bai01/Thuvien.h
+++ 1812756_Lab11/Lab01_P2_Bai01/Lab01_P2_Bai01/Thuvien.h
@@ -1,6 +1,7 @@
 #define MAX 100
 
 void File_Array(char *filename, int a[MAX], int &n);
+int File_Array_2(char *filename, int a[MAX], int &n);
 
 void File_Array(char *filename, int a[MAX], int &n)
 {
@@ -17,4 +18,19 @@ void File_Array(char *filename, int a[MAX], int &n)
 		in >> a[i];
 	in.close();
 }
+
+int File_Array_2(char *filename, int a[MAX], int &n)
+{
+	ifstream in(filename);
+	if (!in)
+		return 0;
+	in >> n;
+	for (int i = 0; i < n; i++)
+		in >> a[i];
+	in.close();
+	return 1;
+}
+
+
+
 
